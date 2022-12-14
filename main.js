30fa3c384ec125908550bbc50e10c7de90b200e4
Kara Anderson <kanderson8585@gmail.com>
2022-12-14 12:28:37 -0700

    Add functionality to recieve message button

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 // List of messages:
-var affirmationArray = ["I forgive myself and set myself free.",
-    "I believe I can be all that I want to be.",
-    "I am in the process of becoming the best version of myself.",
-    "I have the freedom & power to create the life I desire.",
-    "I choose to be kind to myself and love myself unconditionally.",
-    "My possibilities are endless.",
-    "I am worthy of my dreams.",
-    "I am enough.",
-    "I deserve to be healthy and feel good.",
-    "I am full of energy and vitality and my mind is calm and peaceful.",
-    "Every day I am getting healthier and stronger.",
-    "I honor my body by trusting the signals that it sends me.",
-    "I manifest perfect health by making smart choices."]
+var affirmationArray = ["-I forgive myself and set myself free.",
+    "-I believe I can be all that I want to be.",
+    "-I am in the process of becoming the best version of myself.",
+    "-I have the freedom & power to create the life I desire.",
+    "-I choose to be kind to myself and love myself unconditionally.",
+    "-My possibilities are endless.",
+    "-I am worthy of my dreams.",
+    "-I am enough.",
+    "-I deserve to be healthy and feel good.",
+    "-I am full of energy and vitality and my mind is calm and peaceful.",
+    "-Every day I am getting healthier and stronger.",
+    "-I honor my body by trusting the signals that it sends me.",
+    "-I manifest perfect health by making smart choices."]
 var mantraArray = ["Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.",
     "Do not let yesterday take up too much of today.",
     "Every day is a second chance.",
@@ -31,11 +31,22 @@ var mantraArray = ["Breathing in, I send myself love. Breathing out, I send love
 var yellowDude = document.querySelector(".yellow-dude")
 var recieveMessageButton = document.querySelector(".recieve-message-button")
 var messageText = document.querySelector(".message-text")
+var radioButtonMantra = document.getElementById("mantra")
+var radioButtonAffirmation = document.getElementById("affirmation")
 // Event listeners:
 recieveMessageButton.addEventListener("click", hideYellowDude)
 // Event handlers:
 function hideYellowDude() {
     yellowDude.style.display = "none"
-    messageText.innerText = "cabbage"
-    console.log("cabbage")
+    if(radioButtonMantra.checked === true) {
+        messageText.innerText = randomMantra()
+    } else {
+        messageText.innerText = randomAffirmation()
+    }
+}
+function randomAffirmation() {
+    return affirmationArray[Math.floor(Math.random() * affirmationArray.length)]
+}
+function randomMantra() {
+    return mantraArray[Math.floor(Math.random() * mantraArray.length)]
 }
